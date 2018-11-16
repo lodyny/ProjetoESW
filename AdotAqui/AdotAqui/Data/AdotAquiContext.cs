24240573eaec7d81ab740f31682d4988b7288942
César Nero <cesar.augusto.nero@hotmail.com>
2018-11-16 01:34:59 +0000

    Layout&Auth

---
+++ AdotAqui/AdotAqui/Data/AdotAquiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdotAqui.Models
+{
+    public class AdotAquiContext : DbContext
+    {
+        public AdotAquiContext (DbContextOptions<AdotAquiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AdotAqui.Models.User> Users { get; set; }
+    }
+}
