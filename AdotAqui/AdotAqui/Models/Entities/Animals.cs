55a8a51224bf0781bead88022316d91bd8778bbc
César Nero <cesar.augusto.nero@hotmail.com>
2018-12-20 02:37:04 +0000

    Validations

--- AdotAqui/AdotAqui/Models/Entities/Animals.cs
+++ AdotAqui/AdotAqui/Models/Entities/Animals.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdotAqui.Models.Entities
 {
     public partial class Animal
     {
         public int AnimalId { get; set; }
+
+        [Required(ErrorMessage = "Error_NameRequired")]
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "Error_NameLengthInvalid")]
+        [Display(Prompt = "Label_Name")]
         public string Name { get; set; }
+
         public int? UserId { get; set; }
-        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Error_GenderRequired")]
+        [StringLength(1, ErrorMessage = "Error_GenderLengthInvalid")]
+        public char Gender { get; set; }
+
         public int BreedId { get; set; }
+
+        [Required(ErrorMessage = "Error_HeightRequired")]
         public double Height { get; set; }
+
+        [Required(ErrorMessage = "Error_WeightRequired")]
         public double Weight { get; set; }
+
+        [Required(ErrorMessage = "Error_BirthdateRequired")]
+        [DataType(DataType.DateTime)]
+        [Display(Prompt = "Label_Birthday")]
         public DateTime? Birthday { get; set; }
+
         public string Details { get; set; }
-        public byte[] Image { get; set; }
+        public string Image { get; set; }
         public AnimalBreed Breed { get; set; }
     }
 }
