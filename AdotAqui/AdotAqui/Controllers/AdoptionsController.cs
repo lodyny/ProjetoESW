87bb49dd8540b19ca1a148afad79c907fd78bb1c
Dfons25 <davidmgafonso@gmail.com>
2019-02-06 14:40:45 +0000

    up

--- AdotAqui/AdotAqui/Controllers/AdoptionsController.cs
+++ AdotAqui/AdotAqui/Controllers/AdoptionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AdotAqui.Data;
 using AdotAqui.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
@@ -12,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AdotAqui.Controllers
 {
+    
     public class AdoptionsController : Controller
     {
 
@@ -27,6 +29,7 @@ namespace AdotAqui.Controllers
 
 
         // GET: Adoptions
+        [Authorize(Roles = "Administrator")]
         public async Task<ActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -40,6 +43,42 @@ namespace AdotAqui.Controllers
             }
             return View(adoptions);
         }
+
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            AdoptionRequests adoptionRequests = await _context.AdoptionRequests.FindAsync(id);
+
+            if (adoptionRequests == null)
+                return NotFound();
+
+            var rqf = Request.HttpContext.Features.Get<IRequestCultureFeature>();
+            var culture = rqf.RequestCulture.Culture;
+            
+            var adoptions = _context.AdoptionRequests.Include(a => a.User)
+                                                    .Include(a => a.Animal)
+                                                    .Include(a => a.AdoptionLogs)
+                                                    .ThenInclude(p => p.AdoptionState);
+            /*     var statesHistory = from c in _context.AdoptionLogs select c;
+                 statesHistory = statesHistory.Where(s => s.AdoptionRequestId.Equals(id));
+                 /* foreach (AdoptionLogs com in statesHistory)
+                  {
+                      var user = _context.Users.Where(u => u.Id.Equals(com.UserId)).First();
+                      com.SetEmail(user.Email);
+                      com.SetUserName(user.Name);
+                  }
+
+                 IQueryable<AdoptionRequests> vs = _context.AdoptionRequests.Include(a => a.User)
+                                                                       .Include(a => a.Animal)
+                                                                       .Include(a => a.AdoptionLogs)
+                                                                         .ThenInclude(p => p.AdoptionState);*/
+
+            return View(adoptions);
+        }
+        
 
         /*[HttpPost]
         [ValidateAntiForgeryToken]
