09e7784aa433ed14fcdbdbe06ddfc8b548d70fcb
Rúben Ferreira <170221085@estudantes.ips.pt>
2019-02-07 22:42:11 +0000

    Added dashboard Area

---
+++ AdotAqui/AdotAqui/Areas/Statistics/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdotAqui.Areas.Statistics.Controllers
+{
+    [Area("Statistics")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
