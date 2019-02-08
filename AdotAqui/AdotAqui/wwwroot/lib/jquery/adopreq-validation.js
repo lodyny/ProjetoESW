7b4fdf94fe9f5ec27d071e5e2c6e090aa90aee86
Dfons25 <davidmgafonso@gmail.com>
2019-02-08 21:21:06 +0000

    push documentado

--- AdotAqui/AdotAqui/wwwroot/lib/jquery/adopreq-validation.js
+++ AdotAqui/AdotAqui/wwwroot/lib/jquery/adopreq-validation.js
@@ -3,6 +3,30 @@
 
 $(document).ready(function(){
 
+	$.fn.datepicker.dates['pt'] = {
+		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
+		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+		today: "Hoje",
+		monthsTitle: "Meses",
+		clear: "Limpar",
+		format: "dd/mm/yyyy"
+	};
+	
+	$.fn.datepicker.dates['en'] = {
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    daysShort: ["Sud", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    daysMin: ["Sn", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    today: "Today",
+    clear: "Clear",
+    format: "mm/dd/yyyy",
+    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+    weekStart: 0
+	};
 
 	$('.dates-vis').hide();
 	
@@ -23,13 +47,12 @@ $(document).ready(function(){
 		startDate: 'now',
 		autoclose: true
 	})
+	.datepicker('setDate', '')
 	.on('changeDate', function (selected) {
 		var maxDate = new Date(selected.date.valueOf());
 		$('#datepicker_start').datepicker('setEndDate', maxDate);
 	});
-	$('.input-daterange input').each(function() {
-    $(this).datepicker('clearDates');
-});
+
 
 $('#adopt-choice').change(function() {
   if ($('#adopt-choice').prop('selectedIndex') == 0){
@@ -38,5 +61,21 @@ $('#adopt-choice').change(function() {
 	  $('.dates-vis').show();
   };
 });
+
+checkLanguage();
+
+$('#select2-culture-dropdown-container').change(function() {
+  checkLanguage();
 });
 
+});
+
+
+
+function checkLanguage(){if ($('#culture-dropdown').select2('data')[0].id  == "pt-PT"){
+	$("#datepicker_start").datepicker({language: 'pt'});
+	console.log('here');
+  } else {
+	$("#datepicker_start").datepicker({language: 'en'});
+	console.log('here2');
+  };}
