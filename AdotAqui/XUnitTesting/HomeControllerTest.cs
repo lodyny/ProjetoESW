58b159c6565f56d4211fe19d0190c5135b2cb167
Rúben Ferreira <170221085@estudantes.ips.pt>
2019-02-11 04:41:15 +0000

    Unit Testing

--- AdotAqui/XUnitTesting/HomeControllerTest.cs
+++ AdotAqui/XUnitTesting/HomeControllerTest.cs
@@ -9,6 +9,10 @@ using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Moq;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http.Features;
+using System.Globalization;
 
 namespace XUnitTesting
 {
@@ -42,16 +46,19 @@ namespace XUnitTesting
         [Fact]
         public void Index_ReturnsViewResult()
         {
+            var ctxAccessor = buildFakeHttpContextAccesor(new RequestCulture("pt-PT", "pt-PT"));
+            HomeController controller = new HomeController(_context, ctxAccessor);
+            var result = controller.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+        }
+
+        public HttpContextAccessor buildFakeHttpContextAccesor(RequestCulture requestCulture)
+        {
             var ctxAccessor = new HttpContextAccessor();
             var httpContext = new DefaultHttpContext();
-
+            httpContext.Features.Set<IRequestCultureFeature>(new RequestCultureFeature(requestCulture, new AcceptLanguageHeaderRequestCultureProvider()));
             ctxAccessor.HttpContext = httpContext;
-
-            HomeController controller = new HomeController(_context, ctxAccessor);
-
-            var result = controller.Index();
-
-            var viewResult = Assert.IsType<ViewResult>(result);
+            return ctxAccessor;
         }
 
 
